cca1a798fb0e41a9f1e2d37453ed1fdd7f70115a
beloveyeboah <beloveyeboah@gmail.com>
2024-04-03 14:26:50 -0700

    finally

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,33 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+# Bash script that sets up your web servers for the deployment of web_static
+# 1. Install Nginx if it not already installed
+# 2. Create the folder /data/ if it doesn’t already exist
+# 3. Create the folder /data/web_static/ if it doesn’t already exist
+# 4. Create the folder /data/web_static/releases/ if it doesn’t already exist
+# 5. Create the folder /data/web_static/shared/ if it doesn’t already exist
+# 6. Create the folder /data/web_static/releases/test/ if it doesn’t already exist
+# 7. Create a fake HTML file /data/web_static/releases/test/index.html
+# (with simple content, to test your Nginx configuration)
+# 8. Create a symbolic link /data/web_static/current linked to the
+# /data/web_static/releases/test/ folder. If the symbolic link already exists,
+# it should be deleted and recreated every time the script is ran.
+# Give ownership of the /data/ folder to the ubuntu user AND group
+# (you can assume this user and group exist).
+# This should be recursive; everything inside
+# should be created/owned by this user/group.
+# 9. Update the Nginx configuration to serve the content of
+# /data/web_static/current/ to hbnb_static (ex: https://mydomainname.tech/hbnb_static).
+# Don’t forget to restart Nginx after updating the configuration:
+# Use alias inside your Nginx configuration
 
-
-# 1. install nginx
+# 0. update the machine
 sudo apt-get update
-sudo apt-get -y install nginx
 
-#.2 create required directoris
+# 1. install nginx if it not installed
+which nginx > /dev/null || sudo apt-get install -y nginx
+#  sudo ufw allow 'Nginx HTTP'
+
+# 2 - 6. create requiured directories
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
@@ -14,25 +35,33 @@ sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 
+# 7. create a random html page
 
-#. create a fake html page
-sudo echo "<html>
+echo "
+<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+</html>"| sudo tee /data/web_static/releases/test/index.html
 
-
-#. 4 create symbolic likn
+# 8. creating a symbolic lnk
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+# granting ownership to userand group
 
-#. 5 ceate user
+
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/listen 80 default_server/a \tlocation /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
-# text nginx
+# backup nginx default
 
-sudo ngix -t
+#. serving the content on the serverwith nginx
+
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
+# test nginx
+sudo nginx -t
+
+#  restart nginx
 sudo service nginx restart
+-- INSERT --
