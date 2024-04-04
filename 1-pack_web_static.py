e4de358602a18e70e27db9437d7e14f6b0eec52c
beloveyeboah <beloveyeboah@gmail.com>
2024-04-04 15:59:53 -0700

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python3
-"""  Fabric script that generates a .tgz archive
-from the contents of the web_static
 """
+    Generates a .tgz archive from contents of
+    the web_static folder of your AirBnB Clone
+    repo, using the function do_pack.
 
+    def do_pack():
+    All files must be added to the final archive
+    and stored in the folder "versions"
+
+    The name of the archive created must be
+    web_static_<year><month><day><hour><minute><second>.tgz
+
+    The function do_pack must return the archive
+    path if the archive has been correctly generated.
+    Otherwise, it should return None
+"""
+import datetime
 from fabric.api import local
-from time import strftime
-from datetime import date
+from os.path import isdir
 
 
 def do_pack():
-     """
-    generate tgz archive using fabric
-    """
-
-    filename = strftime("%Y%m%d%H%M%S")
+    """Compresses the web_static folder into a .tgz archive"""
     try:
-        local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/"
-              .format(filename))
-
-        return "versions/web_static_{}.tgz".format(filename)
-
-    except Exception as e:
+        day = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_N = "versions/web_static_{}.tgz".format(day)
+        local("tar -czvf {} web_static".format(file_N))
+        return file_N
+    except FileNotFoundError:
         return None
