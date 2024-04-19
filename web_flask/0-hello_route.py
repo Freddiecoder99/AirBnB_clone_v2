bf8cd6f336be26587aa3b6d4278e6011bc4b234f
beloveyeboah <beloveyeboah@gmail.com>
2024-04-19 12:48:00 -0700

    hello flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,11 +7,13 @@ from flask import Flask
 
 app = Flask("__name__")
 
+
 @app.route("/", strict_slashes=False)
 def display():
     """ the root of the wed page"""
 
     return 'Hello HBNB!'
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000', debug=None)
