fc7ec4010694aac9a4be3476c594afeba5c16f5c
beloveyeboah <beloveyeboah@gmail.com>
2024-04-20 01:52:43 -0700

    Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,6 +4,7 @@
 2. routs / - display 'Hello HBNB!
 3. route /hbnb - dispay 'hbnb '
 4. create a /c/<text> route - display the text on the page
+5. create a /python/<text> route - default text is "is cool"
 """
 
 from flask import Flask
@@ -11,6 +12,8 @@ from flask import Flask
 app = Flask("__name__")
 
 # task 0
+
+
 @app.route("/", strict_slashes=False)
 def display():
     """ the root of the wed page"""
@@ -18,6 +21,8 @@ def display():
     return 'Hello HBNB!'
 
 # task 1
+
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnd():
     """the /hbnb page of the server"""
@@ -25,12 +30,28 @@ def hbnd():
     return 'HBNB'
 
 # task 2
+
+
 @app.route("/c/<text>",  strict_slashes=False)
 def c_text(text):
     """ (replace underscore _ symbols with a space)"""
 
     formated_text = text.replace('_', ' ')
     return "C {}".format(formated_text)
+
+
+# task 3
+
+
+@app.route("/python/", defaults={"text": 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    """display “Python ”, followed by the value of the text variable
+    (replace underscore _ symbols with a space)
+    """
+
+    display_text = text.replace('_', ' ')
+    return "Python {}".format(display_text)
 
 
 if __name__ == "__main__":
