811b1d7bc8713a04ba3718722ca8f1b9f1a5518e
beloveyeboah <beloveyeboah@gmail.com>
2024-04-21 06:34:37 -0700

    5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,80 +1,47 @@
 #!/usr/bin/python3
-""" creating an html with flask
-1. the server listens on 0.0.0.0 on port 5000
-2. routs / - display 'Hello HBNB!
-3. route /hbnb - dispay 'hbnb '
-4. create a /c/<text> route - display the text on the page
-5. create a /python/<text> route - default text is "is cool"
-6. create a /number/<n> route
-"""
-
+"""Flask"""
 from flask import Flask, render_template
-
-app = Flask("__name__")
-
-# task 0
+app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def display():
-    """ the root of the wed page"""
-
-    return 'Hello HBNB!'
-
-# task 1
+@app.route('/', strict_slashes=False)
+def hello():
+    """Return a given string"""
+    return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
-def hbnd():
-    """the /hbnb page of the server"""
-
-    return 'HBNB'
-
-# task 2
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns a given string"""
+    return "HBNB"
 
 
-@app.route("/c/<text>",  strict_slashes=False)
-def c_text(text):
-    """ (replace underscore _ symbols with a space)"""
-
-    formated_text = text.replace('_', ' ')
-    return "C {}".format(formated_text)
+@app.route('/c/<text>', strict_slashes=False)
+def cText(text):
+    """display C followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
 
 
-# task 3
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythonText(text="is cool"):
+    """display Python followed by the value of the text variable"""
+    return "Python {}".format(text.replace("_", " "))
 
 
-@app.route("/python/", defaults={"text": 'is cool'}, strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python_text(text):
-    """display “Python ”, followed by the value of the text variable
-    (replace underscore _ symbols with a space)
-    """
-
-    display_text = text.replace('_', ' ')
-    return "Python {}".format(display_text)
-
-
-# task 4
-
-
-@app.route("/number/<int:n>", strict_slashes=False)
-def is_a_number(n):
-    """ display “n is a number” only if n is an integer
-    """
-
-    return "{} is a number".format(n)
-
-# task 5
+@app.route('/number/<int:n>', strict_slashes=False)
+def isNumber(n):
+    """display “n is a number” only if n is an integer"""
+    if isinstance(n, int):
+        return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n=None):
-    """ display a HTML page only if n is an integer"""
-
+    """display a HTML page only if n is an integer"""
     if isinstance(n, int):
-        return render_template('5-number.html', n=n)
+        return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+    app.run()
